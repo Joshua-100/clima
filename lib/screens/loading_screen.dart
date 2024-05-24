da3020cf771bd2ee0222586230fae99cd3fa75b8
mavrentech <mutenjejoshua@gmail.com>
2024-05-24 16:22:47 +0200

    Added GeoLocator, fixed crashing error by adding <uses-permission android:name=android.permission.ACCESS_FINE_LOCATION /> and <uses-permission android:name=android.permission.ACCESS_COARSE_LOCATION />

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -7,13 +8,31 @@ class LoadingScreen extends StatefulWidget {
 
 class _LoadingScreenState extends State<LoadingScreen> {
   @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getLocation();
+    print("Print Initialising State");
+  }
+
+  void getLocation() async {
+    LocationPermission permission;
+    permission = await Geolocator.checkPermission();
+    permission = await Geolocator.requestPermission();
+    if (permission == LocationPermission.denied) {
+      //nothing
+    }
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.low);
+    print(position);
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: ElevatedButton(
-          onPressed: () {
-            //Get the current location
-          },
+          onPressed: () => print("yesds"),
           child: Text('Get Location'),
         ),
       ),
